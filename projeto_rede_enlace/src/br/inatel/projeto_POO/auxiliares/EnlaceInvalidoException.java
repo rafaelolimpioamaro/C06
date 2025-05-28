b937fac5f1fff33ad25175bc84e81913976ff2eb
rafaelolimpioamaro <160930003+rafaelolimpioamaro@users.noreply.github.com>
2025-05-28 11:53:39 -0300

    Delete projeto_rede_enlace directory

--- projeto_rede_enlace/src/br/inatel/projeto_POO/auxiliares/EnlaceInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package br.inatel.projeto_POO.auxiliares;
-
-public class EnlaceInvalidoException extends Exception {
-    public EnlaceInvalidoException(String mensagem) {
-        super(mensagem);
-    }
-}
--- projeto_rede_enlace/src/br/inatel/projeto_POO/auxiliares/Monitoravel.java
+++
@@ -1,5 +0,0 @@
-package br.inatel.projeto_POO.auxiliares;
-
-public interface Monitoravel {
-    void verificarCondicoesOperacionais();
-}
